38fc559822db87c67a867f2b2775d1c4773cbda9
Umesh Mandal <umesh.mandal619@gmail.com>
2020-06-22 18:46:49 +0545

    Create app.module.ts

---
+++ Angular9/src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { EmployeeComponent } from './employee/employee.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { EditEmpComponent } from './employee/edit-emp/edit-emp.component';
+import { AddEmpComponent } from './employee/add-emp/add-emp.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { EditDepComponent } from './department/edit-dep/edit-dep.component';
+import { AddDepComponent } from './department/add-dep/add-dep.component';
+import { DepartmentService } from './services/department.service';
+import { EmployeeService } from './services/employee.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSortModule } from '@angular/material/sort';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    DepartmentComponent,
+    ShowEmpComponent,
+    EditEmpComponent,
+    AddEmpComponent,
+    ShowDepComponent,
+    EditDepComponent,
+    AddDepComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule, 
+    MatTableModule, 
+    MatButtonModule, 
+    MatIconModule,
+    HttpClientModule,
+    MatSortModule,
+    MatDialogModule,
+    FormsModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  providers: [DepartmentService, EmployeeService],
+  bootstrap: [AppComponent],
+  entryComponents: [AddDepComponent,EditDepComponent, AddEmpComponent, EditEmpComponent],
+})
+export class AppModule { }
