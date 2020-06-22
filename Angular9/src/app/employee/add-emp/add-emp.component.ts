4247c02ccc72565e23838a17e214396e9ec89478
Umesh Mandal <umesh.mandal619@gmail.com>
2020-06-22 18:57:43 +0545

    Create add-emp.component.ts

---
+++ Angular9/src/app/employee/add-emp/add-emp.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { EmployeeService } from 'src/app/services/employee.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NgForm } from '@angular/forms';
+import { element } from 'protractor';
+
+@Component({
+  selector: 'app-add-emp',
+  templateUrl: './add-emp.component.html',
+  styleUrls: ['./add-emp.component.css']
+})
+export class AddEmpComponent implements OnInit {
+
+  constructor(public dialogbox: MatDialogRef<AddEmpComponent>,
+    public service: EmployeeService, 
+    private snackBar: MatSnackBar) { }
+ public listItems: Array<string> = [];
+   ngOnInit(): void {
+     this.resetForm();
+     this.dropdownRefresh();
+   }
+   
+  dropdownRefresh(){
+    this.service.getDepDropDownValues().subscribe(data => {
+      console.log(data);
+      data.forEach(element =>{
+        this.listItems.push(element["DepartmentName"]);
+      })
+    })
+}
+
+   resetForm(form?: NgForm){
+     if(form!=null)
+     form.resetForm();
+ 
+     this.service.formData = {
+       EmployeeID: 0,
+       EmployeeName: '',
+       Department: '',
+       MailID: '',
+       DOJ: null
+     }
+   }
+ 
+   onClose(){
+     this.dialogbox.close();
+     this.service.filter('registert Click');
+   }
+ 
+   onSubmit(form: NgForm){
+   console.log(form.value);
+     this.service.addEmployee(form.value).subscribe((res)=>{
+       this.resetForm();
+       this.snackBar.open(res.toString(), '', {
+         verticalPosition: 'top',
+         duration: 5000
+       });
+     })
+   }
+}
